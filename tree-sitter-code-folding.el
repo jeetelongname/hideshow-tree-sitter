15afeb2f1bbfcb1c44771b354d30e7a5671b3096
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-08-28 01:08:02 +0100

    Inital Commit

---
+++ tree-sitter-code-folding.el
@@ -0,0 +1,18 @@
+;;; tree-sitter-code-folding.el --- add tree sitter based code filding  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Jeetaditya Chatterjee
+
+;; Author: Jeetaditya Chatterjee <jeetelongname@gmail.com>
+;; Keywords: languages, faces,
+;;; Commentary:
+;;
+;;; Code:
+(require 'tree-sitter)
+
+(defgroup tree-sitter-code-folding nil
+  "Tree sitter based code folding."
+  :group 'tree-sitter)
+
+(provide 'tree-sitter-code-folding)
+;;; tree-sitter-code-folding.el ends here
+
